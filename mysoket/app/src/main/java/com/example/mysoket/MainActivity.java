7f149681db0ccba38d34cc21bc15bfb631206b1a
japanoxx <japanoxx@gmail.com>
2020-03-09 14:25:38 +0900

    socket통신 실습
    소켓통신 실습

---
+++ mysoket/app/src/main/java/com/example/mysoket/MainActivity.java
@@ -0,0 +1,129 @@
+package com.example.mysoket;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText input1;
+    TextView output1;
+    Handler handler = new Handler();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        input1 = findViewById(R.id.input1);
+        output1 = findViewById(R.id.output1);
+
+        Button sendButton = findViewById(R.id.sendButton);
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String data = input1.getText().toString();
+                new Thread(new Runnable() { //소캣통신을 하기위해서는 쓰레드가 필요하다. 즉시수행할 수 있도록 new Runnalble 매개변수
+                    @Override
+                    public void run() {
+                        send(data);
+                    }
+                }).start();
+
+            }
+        });
+
+        Button startServerButton = findViewById(R.id.startServerButton);
+        startServerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        startServer();
+                    }
+                }).start();
+            }
+
+
+        });
+    }
+
+    public void startServer() {
+        int port = 5001 ;
+        try{
+            ServerSocket server = new ServerSocket(port);
+
+            while(true){
+                Socket sock = server.accept();
+                InetAddress clientHost = sock.getLocalAddress();
+                int clientPort = sock.getPort();
+                println(("클라이언트 연결됨 : " + clientHost + ", "+ clientPort));
+
+                ObjectInputStream instream = new ObjectInputStream(sock.getInputStream());
+                String input = (String) instream.readObject();
+                println("data 받음" + input);
+
+                ObjectOutputStream outstream = new ObjectOutputStream(sock.getOutputStream());
+                outstream.writeObject(input + "from server");
+                outstream.flush();
+                println("데이터 보냄 :");
+
+                sock.close();
+            }
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+
+
+    }
+
+    public void println(final String data){
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                output1.append(data + "\n");
+            }
+        });
+
+
+    }
+
+    public void send(String data){
+        int port = 5001 ;
+        try{
+            Socket sock = new Socket("localhost",port); // 소캣통신을 위한 소캣 생성 , Socket을 쓰면 익셉션이 발생하므로 try catch문을 사용한다
+            ObjectOutputStream outputstream =  new ObjectOutputStream(sock.getOutputStream());
+            outputstream.writeObject(data);
+            outputstream.flush();
+
+            //서버응답처리
+            ObjectInputStream instream = new ObjectInputStream(sock.getInputStream());
+            String input = (String) instream.readObject();
+            sock.close();
+
+
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+}
+
+
+
+
